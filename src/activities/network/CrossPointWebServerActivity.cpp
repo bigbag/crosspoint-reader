9e166dde69b169bbbbc0eb5044a88efe6d9a96dd
Dave Allie <dave@daveallie.com>
2026-01-04 15:08:32 +1000

    Remove authentication type from hotspot QR code (#235)
    ## Summary
    
    * Remove the `T` parameter (authentication type), and `P` parameter
    (password) for the SoftAP wifi config QR code
    
    ## Additional Context
    
    * It is optional according to the spec:
    https://www.wi-fi.org/system/files/WPA3%20Specification%20v3.2.pdf#page=25
    so should either be omitted or set to nopass
    * Fixes https://github.com/daveallie/crosspoint-reader/issues/229

--- src/activities/network/CrossPointWebServerActivity.cpp
+++ src/activities/network/CrossPointWebServerActivity.cpp
@@ -394,7 +394,7 @@ void CrossPointWebServerActivity::renderServerRunning() const {
     renderer.drawCenteredText(THEME.smallFontId, startY + LINE_SPACING * 3,
                               "or scan QR code with your phone to connect to Wifi.", THEME.primaryTextBlack, REGULAR);
     // Show QR code for URL
-    std::string wifiConfig = std::string("WIFI:T:WPA;S:") + connectedSSID + ";P:" + "" + ";;";
+    const std::string wifiConfig = std::string("WIFI:S:") + connectedSSID + ";;";
     drawQRCode(renderer, (480 - 6 * 33) / 2, startY + LINE_SPACING * 4, wifiConfig);
 
     startY += 6 * 29 + 3 * LINE_SPACING;
